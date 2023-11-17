d43dcbe9b6a3a54bd8f2ac01b50c178ee06838ce
Linnea1 <linnea.a.14@gmail.com>
2023-11-17 14:05:10 +0100

    WelcomePage

--- javaScript/loginRegister.js
+++ javaScript/loginRegister.js
@@ -2,11 +2,13 @@ function renderWelcomePage(){
 main.innerHTML=`
     <div class="welcomePageWrapper">
         <div class="welcomePagePicture"></div>
-        <h1>Hello!</h1>
-        <p>Lorem ipsum dolor sit amet consectetur. Phasellus hac est pellentesque elementum massa vel.</p>
-        <div class="loginRegisterButtonContainer">
-            <div class="loginButton">Log In</div>
-            <div class="registerButton">Create Account</div>
+        <div class="welcomePageContentContainer">
+            <h1>Hello!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur. Phasellus hac est pellentesque elementum massa vel.</p>
+            <div class="loginRegisterButtonContainer">
+                <div class="loginButton">Log In</div>
+                <div class="registerButton">Create Account</div>
+            </div>
         </div>
     </div>
 `
