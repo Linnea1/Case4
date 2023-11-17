014fbbef76a6605b95524aaa59a28734fde53458
Grace <yintingchancytg@gmail.com>
2023-11-17 11:11:42 +0100

    Marked up data.js

---
+++ data.js
@@ -0,0 +1,108 @@
+const awards = [
+  {
+    name: "Oscars",
+    date: "10 March, 2024",
+    categories: [
+      {
+        name: "Best Picture",
+        nominees: [
+          {
+            film: "All Quiet on the Western Front",
+            picture: "url",
+          },
+          {
+            film: "Avatar: The Way of Water",
+            picture: "url",
+          },
+          {
+            film: "The Banshees of Inisherin",
+            picture: "url",
+          },
+          {
+            film: "Elvis",
+            picture: "url",
+          },
+          {
+            film: "Everything Everywhere All at Once",
+            picture: "url",
+          },
+          {
+            film: "The Fabelmans",
+            picture: "url",
+          },
+          {
+            film: "Tár",
+            picture: "url",
+          },
+          {
+            film: "Top Gun: Maverick",
+            picture: "url",
+          },
+          {
+            film: "Triangle of Sadness",
+            picture: "url",
+          },
+          {
+            film: "Women Talking",
+            picture: "url",
+          },
+        ],
+        winner: "Everything Everywhere All at Once",
+      },
+      {
+        name: "Best Actor",
+        nominees: [
+          {
+            name: "Austin Butler",
+            film: "Elvis",
+            picture: "url",
+          },
+          {
+            name: "Colin Farrell",
+            film: "The Banshees of Inisherin",
+            picture: "url",
+          },
+          {
+            name: "Brendan Fraser",
+            film: "The Whale",
+            picture: "url",
+          },
+          {
+            name: "Paul Mescal",
+            film: "Aftersun",
+            picture: "url",
+          },
+          {
+            name: "Bill Nighy",
+            film: "Living",
+            picture: "url",
+          },
+        ],
+        winner: "Brendan Fraser",
+      },
+      {
+        name: "Best Actress",
+        nominees: [],
+        winner: "",
+      },
+      {
+        name: "Best Supporting Actor",
+        nominees: [],
+        winner: "",
+      },
+      {
+        name: "Best Supporting Actress",
+        nominees: [],
+        winner: "",
+      },
+    ],
+  },
+  {
+    name: "Emmy",
+    categories: [],
+  },
+  {
+    name: "Grammy",
+    categories: [],
+  },
+];
