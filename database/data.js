c002026db2204eb56e915e278478deeb595d2107
Grace <yintingchancytg@gmail.com>
2023-11-17 20:04:49 +0100

    Updated the data.js

--- database/data.js
+++ database/data.js
@@ -1,108 +1,650 @@
 const awards = [
   {
-    name: "Oscars",
+    award: "Emmys",
+    date: "15 January, 2024",
+    categories: [
+      {
+        category: "Outstanding Drama Series",
+        nominees: [
+          {
+            series: "Andor",
+            uri: "Andor.jpeg",
+          },
+          {
+            series: "Better Call Saul",
+            uri: "Better-Call-Saul.jpeg",
+          },
+          {
+            series: "House of the Dragon",
+            uri: "House-of-the-Dragon.jpeg",
+          },
+          {
+            series: "Succession",
+            uri: "Succession.jpeg",
+          },
+          {
+            series: "The Crown",
+            uri: "The-Crown.jpeg",
+          },
+          {
+            series: "The Last Of Us",
+            uri: "The-Last-Of-Us.jpeg",
+          },
+          {
+            series: "The White Lotus",
+            uri: "The-White-Lotus.jpeg",
+          },
+          {
+            series: "Yellowjackets",
+            uri: "Yellowjackets.jpeg",
+          },
+        ],
+        winner: "Succession",
+      },
+      {
+        category: "Outstanding Comedy Series",
+        nominees: [
+          {
+            series: "Abbott Elementary",
+            uri: "Abbott-Elementary.jpeg",
+          },
+          {
+            series: "Barry",
+            uri: "Barry.jpeg",
+          },
+          {
+            series: "Jury Duty",
+            uri: "Jury-Duty.jpeg",
+          },
+          {
+            series: "Only Murders In The Building",
+            uri: "Only-Murders-In-The-Building.jpeg",
+          },
+          {
+            series: "Ted Lasso",
+            uri: "Ted-Lasso.jpeg",
+          },
+          {
+            series: "The Bear",
+            uri: "The-Bear.jpeg",
+          },
+          {
+            series: "The Marvelous Mrs. Maisel",
+            uri: "The-Marvelous-Mrs-Maisel.jpeg",
+          },
+          {
+            series: "Wednesday",
+            uri: "Wednesday.jpeg",
+          },
+        ],
+        winner: "The Bear",
+      },
+      {
+        category: "Outstanding Lead Actor in a Drama Series",
+        nominees: [
+          {
+            name: "Jeff Bridges",
+            series: "The Old Man",
+            uri: "Jeff-Bridges.jpeg",
+          },
+          {
+            name: "Brian Cox",
+            series: "Succession",
+            uri: "Brian-Cox.jpeg",
+          },
+          {
+            name: "Kieran Culkin",
+            series: "Succession",
+            uri: "Kieran-Culkin.jpeg",
+          },
+          {
+            name: "Bob Odenkirk",
+            series: "Better Call Saul",
+            uri: "Bob-Odenkirk.jpeg",
+          },
+          {
+            name: "Pedro Pascal",
+            series: "The Last Of Us",
+            uri: "Pedro-Pascal.jpeg",
+          },
+          {
+            name: "Jeremy Strong",
+            series: "Succession",
+            uri: "Jeremy-Strong.jpeg",
+          },
+        ],
+        winner: "Kieran Culkin",
+      },
+      {
+        category: "Outstanding Lead Actress in a Drama Seriess",
+        nominees: [
+          {
+            name: "Sharon Horgan",
+            series: "Bad Sisters",
+            uri: "Sharon-Horgan.jpeg",
+          },
+          {
+            name: "Melanie Lynskey",
+            series: "Yellowjackets",
+            uri: "Melanie-Lynskey.jpeg",
+          },
+          {
+            name: "Elisabeth Moss",
+            series: "The Handmaid's Tale",
+            uri: "Elisabeth-Moss.jpeg",
+          },
+          {
+            name: "Bella Ramsey",
+            series: "The Last Of Us",
+            uri: "Bella-Ramsey.jpeg",
+          },
+          {
+            name: "Keri Russell",
+            series: "The Diplomat",
+            uri: "Keri-Russell.jpeg",
+          },
+          {
+            name: "Sarah Snook",
+            series: "Succession",
+            uri: "Sarah-Snook.jpeg",
+          },
+        ],
+        winner: "Sarah Snook",
+      },
+      {
+        category: "Outstanding Lead Actress in a Comedy Series",
+        nominees: [
+          {
+            name: "Christina Applegate",
+            series: "Dead To Me",
+            uri: "Christina-Applegate.jpeg",
+          },
+          {
+            name: "Rachel Brosnahan",
+            series: "The Marvelous Mrs. Maisel",
+            uri: "Rachel-Brosnahan.jpeg",
+          },
+          {
+            name: "Quinta Brunson",
+            series: "Abbott Elementary",
+            uri: "Quinta-Brunson.jpeg",
+          },
+          {
+            name: "Natasha Lyonne",
+            series: "Poker Face",
+            uri: "Natasha-Lyonne.jpeg",
+          },
+          {
+            name: "Jenna Ortega",
+            series: "Wednesday",
+            uri: "Jenna-Ortega.jpeg",
+          },
+        ],
+        winner: "Quinta Brunson",
+      },
+      {
+        category: "Outstanding Lead Actor in a Comedy Series",
+        nominees: [
+          {
+            name: "Bill Hader",
+            series: "Barry",
+            uri: "Bill-Hader.jpeg",
+          },
+          {
+            name: "Jason Segel",
+            series: "Shrinking",
+            uri: "Jason-Segel.jpeg",
+          },
+          {
+            name: "Martin Short",
+            series: "Only Murders In The Building",
+            uri: "Martin-Short.jpeg",
+          },
+          {
+            name: "Jason Sudeikis",
+            series: "Ted Lasso",
+            uri: "Jason-Sudeikis.jpeg",
+          },
+          {
+            name: "Jeremy Allen White",
+            series: "The Bear",
+            uri: "Jeremy-Allen-White.jpeg",
+          },
+        ],
+        winner: "Jeremy Allen White",
+      },
+    ],
+  },
+  {
+    award: "Grammys",
+    date: "4 February, 2024",
+    categories: [
+      {
+        category: "Record of the Year",
+        nominees: [
+          {
+            name: "ABBA",
+            song: "Don’t Shut Me Down",
+            uri: "ABBA.jpeg",
+          },
+          {
+            name: "Adele",
+            song: "Easy on Me",
+            uri: "Adele.jpeg",
+          },
+          {
+            name: "Beyoncé",
+            song: "Break My Soul",
+            uri: "Beyoncé.jpeg",
+          },
+          {
+            name: "Brandi Carlile Featuring Lucius",
+            song: "You and Me on the Rock",
+            uri: "Brandi-Carlile-Featuring-Lucius.jpeg",
+          },
+          {
+            name: "Doja Cat",
+            song: "Woman",
+            uri: "Doja-Cat.jpeg",
+          },
+          {
+            name: "Harry Styles",
+            song: "As It Was",
+            uri: "Harry-Styles.jpeg",
+          },
+          {
+            name: "Kendrick Lamar",
+            song: "The Heart Part 5",
+            uri: "Kendrick-Lamar.jpeg",
+          },
+          {
+            name: "Lizzo",
+            song: "About Damn Time",
+            uri: "Lizzo.jpeg",
+          },
+          {
+            name: "Mary J. Blige",
+            song: "Good Morning Gorgeous",
+            uri: "Mary-J-Blige.jpeg",
+          },
+          {
+            name: "Steve Lacy",
+            song: "Bad Habit",
+            uri: "Steve-Lacy.jpeg",
+          },
+        ],
+        winner: "Lizzo",
+      },
+      {
+        category: "Song Of The Year",
+        nominees: [
+          {
+            name: "Adele",
+            song: "Easy on Me",
+            uri: "Adele.jpeg",
+          },
+          {
+            name: "Beyoncé",
+            song: "Break My Soul",
+            uri: "Beyoncé.jpeg",
+          },
+          {
+            name: "Bonnie Raitt",
+            song: "Just Like That",
+            uri: "Bonnie-Raitt.jpeg",
+          },
+          {
+            name: "DJ Khaled Featuring Rick Ross, Lil Wayne, Jay-Z, John Legend & Fridayy",
+            song: "God Did",
+            uri: "DJ-Khaled-Featuring-Rick-Ross.jpeg",
+          },
+          {
+            name: "Gayle",
+            song: "ABCDEFU",
+            uri: "Gayle.jpeg",
+          },
+          {
+            name: "Harry Styles",
+            song: "As It Was",
+            uri: "Harry-Styles.jpeg",
+          },
+          {
+            name: "Kendrick Lamar",
+            song: "The Heart Part 5",
+            uri: "Kendrick-Lamar.jpeg",
+          },
+          {
+            name: "Lizzo",
+            song: "About Damn Time",
+            uri: "Lizzo.jpeg",
+          },
+          {
+            name: "Steve Lacy",
+            song: "Bad Habit",
+            uri: "Steve-Lacy.jpeg",
+          },
+          {
+            name: "Taylor Swift",
+            song: "All Too Well: The Short Film",
+            uri: "Taylor-Swift.jpeg",
+          },
+        ],
+        winner: "Bonnie Raitt",
+      },
+      {
+        category: "Best New Artist",
+        nominees: [
+          {
+            name: "Anitta",
+            uri: "Anitta.jpeg",
+          },
+          {
+            name: "Domi & JD Beck",
+            uri: "Domi-&-JD-Beck.jpeg",
+          },
+          {
+            name: "Latto",
+            uri: "Latto.jpeg",
+          },
+          {
+            name: "Måneskin",
+            uri: "Måneskin.jpeg",
+          },
+          {
+            name: "Molly Tuttle",
+            uri: "Molly-Tuttle.jpeg",
+          },
+          {
+            name: "Muni Long",
+            uri: "Muni-Long.jpeg",
+          },
+          {
+            name: "Omar Apollo",
+            uri: "Omar-Apollo.jpeg",
+          },
+          {
+            name: "Samara Joy",
+            uri: "Samara-Joy.jpeg",
+          },
+          {
+            name: "Tobe Nwigwe",
+            uri: "Tobe-Nwigwe.jpeg",
+          },
+          {
+            name: "Wet Leg",
+            uri: "Wet-Leg.jpeg",
+          },
+        ],
+        winner: "Samara Joy",
+      },
+      {
+        category: "Album Of The Year",
+        nominees: [
+          {
+            name: "ABBA",
+            album: "Voyage",
+            uri: "ABBA.jpeg",
+          },
+          {
+            name: "Adele",
+            album: "30",
+            uri: "Adele.jpeg",
+          },
+          {
+            name: "Bad Bunny",
+            album: "Un Verano Sin Ti",
+            uri: "Bad-Bunny.jpeg",
+          },
+          {
+            name: "Beyoncé",
+            album: "Renaissance",
+            uri: "Beyoncé.jpeg",
+          },
+          {
+            name: "Brandi Carlile",
+            album: "In These Silent Days",
+            uri: "Brandi-Carlile.jpeg",
+          },
+          {
+            name: "Coldplay",
+            album: "Music of the Spheres",
+            uri: "Coldplay.jpeg",
+          },
+          {
+            name: "Harry Styles",
+            album: "Harry’s House",
+            uri: "Harry-Styles.jpeg",
+          },
+          {
+            name: "Kendrick Lamar",
+            album: "Mr. Morale & the Big Steppers",
+            uri: "Kendrick-Lamar.jpeg",
+          },
+          {
+            name: "Lizzo",
+            album: "Special",
+            uri: "Lizzo.jpeg",
+          },
+          {
+            name: "Mary J. Blige",
+            album: "Good Morning Gorgeous (Deluxe)",
+            uri: "Mary-J-Blige.jpeg",
+          },
+        ],
+        winner: "Harry’s House",
+      },
+      {
+        category: "Best Pop Solo Performance",
+        nominees: [
+          {
+            name: "Adele",
+            song: "Easy on Me",
+            uri: "Adele.jpeg",
+          },
+          {
+            name: "Bad Bunny",
+            song: "Moscow Mule",
+            uri: "Bad-Bunny.jpeg",
+          },
+          {
+            name: "Doja Cat",
+            song: "Woman",
+            uri: "Doja-Cat.jpeg",
+          },
+          {
+            name: "Harry Styles",
+            song: "As It Was",
+            uri: "Harry-Styles.jpeg",
+          },
+          {
+            name: "Lizzo",
+            song: "About Damn Time",
+            uri: "Lizzo.jpeg",
+          },
+          {
+            name: "Steve Lacy",
+            song: "Bad Habit",
+            uri: "Steve-Lacy.jpeg",
+          },
+        ],
+        winner: "Adele",
+      },
+    ],
+  },
+  {
+    award: "Oscars",
     date: "10 March, 2024",
     categories: [
       {
-        name: "Best Picture",
+        category: "Best Picture",
         nominees: [
           {
             film: "All Quiet on the Western Front",
-            picture: "url",
+            uri: "All-Quiet-on-the-Western-Front.jpeg",
           },
           {
             film: "Avatar: The Way of Water",
-            picture: "url",
+            uri: "Avatar-The-Way-of-Water.jpeg",
           },
           {
             film: "The Banshees of Inisherin",
-            picture: "url",
+            uri: "The-Banshees-of-Inisherin.jpeg",
           },
           {
             film: "Elvis",
-            picture: "url",
+            uri: "Elvis.jpeg",
           },
           {
             film: "Everything Everywhere All at Once",
-            picture: "url",
+            uri: "Everything-Everywhere-All-at-Once.jpeg",
           },
           {
             film: "The Fabelmans",
-            picture: "url",
+            uri: "The-Fabelmans.jpeg",
           },
           {
             film: "Tár",
-            picture: "url",
+            uri: "Tár.jpeg",
           },
           {
             film: "Top Gun: Maverick",
-            picture: "url",
+            uri: "Top-Gun-Maverick.jpeg",
           },
           {
             film: "Triangle of Sadness",
-            picture: "url",
+            uri: "Triangle-of-Sadness.jpeg",
           },
           {
             film: "Women Talking",
-            picture: "url",
+            uri: "Women-Talking.jpeg",
           },
         ],
         winner: "Everything Everywhere All at Once",
       },
       {
-        name: "Best Actor",
+        category: "Actor in a Leading Role",
         nominees: [
           {
             name: "Austin Butler",
             film: "Elvis",
-            picture: "url",
+            uri: "Austin-Butler.jpeg",
           },
           {
             name: "Colin Farrell",
             film: "The Banshees of Inisherin",
-            picture: "url",
+            uri: "Colin-Farrell.jpeg",
           },
           {
             name: "Brendan Fraser",
             film: "The Whale",
-            picture: "url",
+            uri: "Brendan-Fraser.jpeg",
           },
           {
             name: "Paul Mescal",
             film: "Aftersun",
-            picture: "url",
+            uri: "Paul-Mescal.jpeg",
           },
           {
             name: "Bill Nighy",
             film: "Living",
-            picture: "url",
+            uri: "Bill-Nighy.jpeg",
           },
         ],
         winner: "Brendan Fraser",
       },
       {
-        name: "Best Actress",
-        nominees: [],
-        winner: "",
+        category: "Actress in a Leading Role",
+        nominees: [
+          {
+            name: "Cate Blanchett",
+            film: "Tár",
+            uri: "Cate-Blanchett.jpeg",
+          },
+          {
+            name: "Ana de Armas",
+            film: "Blonde",
+            uri: "Ana-de-Armas.jpeg",
+          },
+          {
+            name: "Andrea Riseborough",
+            film: "To Leslie",
+            uri: "Andrea-Riseborough.jpeg",
+          },
+          {
+            name: "Michelle Williams",
+            film: "The Fabelmans",
+            uri: "Michelle-Williams.jpeg",
+          },
+          {
+            name: "Michelle Yeoh",
+            film: "Everything Everywhere All at Once",
+            uri: "Michelle-Yeoh.jpeg",
+          },
+        ],
+        winner: "Michelle Yeoh",
       },
       {
-        name: "Best Supporting Actor",
-        nominees: [],
-        winner: "",
+        category: "Actor in a Supporting Role",
+        nominees: [
+          {
+            name: "Brendan Gleeson",
+            film: "The Banshees of Inisherin",
+            uri: "Brendan-Gleeson.jpeg",
+          },
+          {
+            name: "Brian Tyree Henry",
+            film: "Causeway",
+            uri: "Brian-Tyree-Henry.jpeg",
+          },
+          {
+            name: "Judd Hirsch",
+            film: "The Fabelmans",
+            uri: "Judd-Hirsch.jpeg",
+          },
+          {
+            name: "Barry Keoghan",
+            film: "The Banshees of Inisherin",
+            uri: "Barry-Keoghan.jpeg",
+          },
+          {
+            name: "Ke Huy Quan",
+            film: "Everything Everywhere All at Once",
+            uri: "Ke-Huy-Quan.jpeg",
+          },
+        ],
+        winner: "Ke Huy Quan",
       },
       {
-        name: "Best Supporting Actress",
-        nominees: [],
-        winner: "",
+        category: "Actress in a Supporting Role",
+        nominees: [
+          {
+            name: "Angela Bassett",
+            film: "Black Panther: Wakanda Forever",
+            uri: "Angela-Bassett.jpeg",
+          },
+          {
+            name: "Hong Chau",
+            film: "The Whale",
+            uri: "Hong-Chau.jpeg",
+          },
+          {
+            name: "Kerry Condon",
+            film: "The Banshees of Inisherin",
+            uri: "Kerry-Condon.jpeg",
+          },
+          {
+            name: "Jamie Lee Curtis",
+            film: "Everything Everywhere All at Once",
+            uri: "Jamie-Lee-Curtis.jpeg",
+          },
+          {
+            name: "Stephanie Hsu",
+            film: "Everything Everywhere All at Once",
+            uri: "Stephanie-Hsu.jpeg",
+          },
+        ],
+        winner: "Jamie Lee Curtis",
       },
     ],
-  },
-  {
-    name: "Emmy",
-    categories: [],
-  },
-  {
-    name: "Grammy",
-    categories: [],
   },
 ];
