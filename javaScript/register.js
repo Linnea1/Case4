580eb46c7fba50ff5e59caadd5ebbea63fcf260d
Grace <yintingchancytg@gmail.com>
2023-11-22 11:55:27 +0100

    Updated buttons

--- javaScript/register.js
+++ javaScript/register.js
@@ -2,7 +2,7 @@ function renderRegisterPage(){
     
     main.innerHTML=`
     <div class="registerPageWrapper">
-        <div class="registerGoBack button">←</div>
+        <div><i class="fa-solid fa-arrow-left"></i></div>
         <div class="registerTextContainer">
             <h1>Hi!</h1>
             <p>Create an account to continue</p>
@@ -13,45 +13,12 @@ function renderRegisterPage(){
             <input type="password" class="password registerBox" name="password" placeholder="Password" required><br>
 
             <p class="registerErrorMessage"></p>
-            <button class="sendRegisterForm" type="button">Continue</button>
+            <button class="btn-main sendRegisterForm" type="button">Continue</button>
         </form>
-        <p class="alreadyAccount button">Already have an account? Log In</p>
+        <footer class="alreadyAccount">Already have an account?
+          <button>Log In</button>
+        </footer>
     </div>
-    `
-    document.querySelector(".sendRegisterForm").addEventListener("click", register);
-    document.querySelector(".registerGoBack").addEventListener("click", renderWelcomePage);
-    document.querySelector(".alreadyAccount").addEventListener("click", renderLoginPage);
-    async function register() {
-        try {
-          var username = document.querySelector(".username").value;
-          var email = document.querySelector(".email").value;
-          var password = document.querySelector(".password").value;
-      
-          var newUser = {
-            username: username,
-            email: email,
-            password: password,
-            totalPoints: 0,
-            groups: []
-          };
-      
-          const response = await fetch("PHP/register.php", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newUser),
-          });
-      
-          const data = await response.json();
-          if (!response.ok){
-            document.querySelector(".registerErrorMessage").textContent=data.error;
-          }else{
-            console.log("Registration successful:", data);
-          }
-        } catch (error) {
-          console.error("Error during registration:", error);
-        }
-    }
-
+  document.querySelector(".fa-arrow-left").addEventListener("click", renderWelcomePage);
+  document.querySelector(".alreadyAccount button").addEventListener("click", renderLoginPage);
 }
