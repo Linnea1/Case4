5168ca285f8b957bb99901e09f08a813959c0c4e
Grace <yintingchancytg@gmail.com>
2023-11-23 09:25:31 +0100

    Removed comments

--- PHP/groupInfo.php
+++ PHP/groupInfo.php
@@ -26,14 +26,11 @@ if (isset($id)) {
     if (trim($user["userId"]) === $id) {
       $userIsFound = $user;
       $userGroups = $user["groups"];
-      // file_put_contents("./dump.txt", json_encode($userGroups));
       foreach($userGroups as $userGroup) {
         foreach($groups as $group) {
-          // file_put_contents("./dump.txt", json_encode($group["groupId"]));
           if ($userGroup === $group["groupId"]) {
             file_put_contents("./dump.txt", "works");
             array_push($groupsFound, $group);
-
           }
         }
       }
