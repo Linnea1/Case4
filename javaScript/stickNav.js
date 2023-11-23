2de02a7f49ea5a24f7d303b81b36e02d59a45ffd
Grace <yintingchancytg@gmail.com>
2023-11-23 09:42:57 +0100

    Added classes to stickyNav

--- javaScript/stickNav.js
+++ javaScript/stickNav.js
@@ -3,10 +3,10 @@
 function stickyNav() {
   return `
     <ul>
-      <li><i class="fa-solid fa-house"></i></li>
-      <li><i class="fa-solid fa-people-group"></i></li>
-      <li><i class="fa-solid fa-user"></i></li>
-      <li><i class="fa-solid fa-gears"></i></li>
+      <li class="home"><i class="fa-solid fa-house"></i></li>
+      <li class="teams"><i class="fa-solid fa-people-group"></i></li>
+      <li class="profile"><i class="fa-solid fa-user"></i></li>
+      <li class="setting"><i class="fa-solid fa-gears"></i></li>
     </ul>
   `;
 }
