21ac1625276d0c366c37f6f068c6a48904004563
Grace <yintingchancytg@gmail.com>
2023-11-23 21:42:50 +0100

    Rendered team pages to different groups

--- javaScript/myTeams.js
+++ javaScript/myTeams.js
@@ -13,12 +13,28 @@ async function teamContent(profileContent) {
   document
     .querySelector(".sticky-nav .home")
     .addEventListener("click", renderHomePage);
+
+  let teams = document.querySelectorAll(".team");
+
+  teams.forEach((team) => {
+    const teamId = team.id;
+    team.addEventListener("click", () => {
+      const selectedTeam = userTeams.find(
+        (element) =>
+          element.groupName.toLowerCase().replace(/\s+/g, "-") === teamId
+      );
+      navigateToTeamPage(selectedTeam);
+    });
+  });
 }
 
 function renderTeams(userTeams) {
   const team = userTeams
     .map(
       (element) => `
+      <button class="team" id="${element.groupName
+        .toLowerCase()
+        .replace(/\s+/g, "-")}">
         <h3>${element.groupName.toUpperCase()}</h3>
         <div class="team-info">
           <p>${element.members.length} members</p>
