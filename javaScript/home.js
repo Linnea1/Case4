32b2e629aa07a8140f16994adf02bcf0bc359328
Grace <yintingchancytg@gmail.com>
2023-11-24 12:07:42 +0100

    Removed awards from home page

--- javaScript/home.js
+++ javaScript/home.js
@@ -16,30 +16,12 @@ async function renderHomePage() {
         </h1>
         <p>Ready to predict like a pro? Let's start the betting frenzy!</p>
       </div>
-      <div class="awards">${renderAwards(awards)}</div>
       <nav class="sticky-nav">${stickyNav()}</nav>
     </div>
   `;
 
   document.querySelector(".fa-user").addEventListener("click", renderProfilePage);
   document.querySelector(".logoutButton").addEventListener("click", logoutFromAccount);
-}
-
-function renderAwards(awards) {
-  const award = awards
-    .map(
-      (element) => `
-        <button class="award-item ${element.award.toLowerCase()}">
-          <h2>${element.award}</h2>
-          <p>${element.date}</p>
-        </button>
-      `
-    )
-    .join("");
-
-  return `
-    ${award}
-  `;
 }
 
 function logoutFromAccount() {
