7e2bfff2633cfdd175dcf0385c573c3a4293da47
Grace <yintingchancytg@gmail.com>
2023-11-24 12:13:44 +0100

    Added access to awards page from my teams page

--- javaScript/myTeams.js
+++ javaScript/myTeams.js
@@ -22,6 +22,8 @@ async function teamContent(profileContent) {
       navigateToTeamPage(selectedTeam);
     });
   });
+
+  document.querySelector(".fa-trophy").addEventListener("click", () => renderAwardsPage(awards));
 }
 
 function renderTeams(userTeams) {
