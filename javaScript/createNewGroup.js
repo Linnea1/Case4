7180d39909e9f90ee983e117665335eb1d89f890
Grace <yintingchancytg@gmail.com>
2023-11-29 11:36:51 +0100

    Added content for createNewGroup page

---
+++ javaScript/createNewGroup.js
@@ -0,0 +1,30 @@
+"use strict";
+
+async function renderCreateNewGroupPage() {
+  main.classList.remove("bg-home");
+  main.innerHTML = `
+    <div class="page-create-new-group">
+      <div class="create-new-group-container">
+        <form class="new-group-form" action="PHP/createNewGroup.php" method="POST">
+          <div class="new-group-name-container">
+            <input type="text" class="new-group-name" placeholder="Enter group name &#xf304;" name="new-group-name" required">
+            <i class="fa-solid fa-pen"></i>
+          </div>
+          <div class="new-group-member-container">
+            <label for="new-group-member">Add group member</label>
+            <div class="input-row">
+              <input type="text" class="new-group-member" placeholder="Enter username" name="new-group-member" oninput="searchUsername()" required>
+              <i class="fa-solid fa-plus"></i>
+            </div>
+            <div class="dropdownList"></div>
+          </div>
+          <div class="group-members-list">
+            <div>Group members</div>
+            <ul></ul>
+          </div>
+        </form>
+        <button class="create-new-group">Done</button>
+      </div>
+    <div>
+  `;
+}
