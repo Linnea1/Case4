49ac3aa3632029cf6c04f298c9fc6998bbe0a75c
Grace <yintingchancytg@gmail.com>
2023-12-06 10:29:01 +0100

    Updated sticky bar in tablet portrait

--- javaScript/stickNav.js
+++ javaScript/stickNav.js
@@ -3,20 +3,21 @@
 function stickyNav() {
   return `
     <ul>
+      <div class="app-logo">G</div>
       <li class="nav-home">
-        <i class="fa-solid fa-house"></i>
+        <i class="fa-solid fa-house nav-icon"></i>
         <p class="nav-icon-name text-house">Home</p>
       </li>
       <li class="nav-groups">
-        <i class="fa-solid fa-people-group"></i>
+        <i class="fa-solid fa-people-group nav-icon"></i>
         <p class="nav-icon-name text-groups">Groups</p>
       </li>
       <li class="nav-awards">
-        <i class="fa-solid fa-trophy"></i>
+        <i class="fa-solid fa-trophy nav-icon"></i>
         <p class="nav-icon-name text-awards">Awards</p>
       </li>
       <li class="nav-profile">
-        <i class="fa-solid fa-user"></i>
+        <i class="fa-solid fa-user nav-icon"></i>
         <p class="nav-icon-name text-profile">Profile</p>
         </li>
     </ul>
