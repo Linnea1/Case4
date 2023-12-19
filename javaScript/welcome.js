21bb1587c8e36b0b7a33aa9ec0a07f003a01f5c5
Grace <yintingchancytg@gmail.com>
2023-12-19 08:53:53 +0100

    Updated css for welcome page

--- javaScript/welcome.js
+++ javaScript/welcome.js
@@ -1,28 +1,29 @@
 function renderWelcomePage() {
   main.innerHTML = `
-        <div class="welcome-container">
-            <h1 class="goldenbet">GoldenBet</h1>
-            <div class="blurred-background"></div>
-            <div class="welcome-text">
-                <div class="web-title">GoldenBet</div>
-                <div class="welcome-content">
-                    <h1>Hello!</h1>
-                    <p>Ready to predict like a pro? Let's start the betting frenzy!</p>
-                </div>
+    <div class="welcome-container backdrop">
+      <h1 class="goldenbet">GoldenBet</h1>
 
-                <div class="tablet-welcome-buttons">
-                    <div class="tablet-login-button button btn-main">Log In</div>
-                    <div class="tablet-register-button button btn-main">Create Account</div>
-                </div>
-            </div>
+      <div class="blurred-background-welcome"></div>
 
-            <div class="welcome-buttons">
-                <div class="login-button button btn-main">Log In</div>
-                <div class="register-button button btn-main">Create Account</div>
-            </div>
+      <div class="welcome-text black-box">
+        <div class="web-title">GoldenBet</div>
+        <div class="welcome-content">
+            <h1>Welcome!</h1>
+            <p>Ready to predict like a pro? Let's start the betting frenzy!</p>
         </div>
 
-    `;
+        <div class="tablet-welcome-buttons">
+            <div class="tablet-login-button button btn-main">Log In</div>
+            <div class="tablet-register-button button btn-main">Create Account</div>
+        </div>
+
+        <div class="welcome-buttons">
+            <div class="login-button button btn-main">Log In</div>
+            <div class="register-button button btn-main">Create Account</div>
+        </div>
+      </div>
+    </div>
+  `;
 
   document
     .querySelector(".login-button")
